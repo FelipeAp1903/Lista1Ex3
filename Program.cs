37d3046cc275f5cbd9058e25cc8c46ddc39c6e3e
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 16:41:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1;
+            Double soma;
+            Double e;
+            Double n2;
+            Double raiz = 1.41;
+            Console.WriteLine("Coloque o valor do Diametro:");
+            n1 = Double.Parse(Console.ReadLine());
+            e = n1 / raiz;
+            soma = n1 / raiz;
+            n2 = soma * e;
+            Console.WriteLine("O valor da Área");
+            Console.WriteLine(n2);
+        }
+    }
+}
